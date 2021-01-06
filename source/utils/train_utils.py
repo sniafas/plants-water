a56cb4057f8be52337faf2325eafd40e5cbb1035
Stavros Niafas <sniafas@tuta.io>
2021-01-06 13:42:12 +0200

    [feat]: loss and metrics

--- source/utils/train_utils.py
+++ source/utils/train_utils.py
@@ -1,4 +1,8 @@
+import tensorflow_addons as tfa
+from tensorflow.keras.losses import CategoricalCrossentropy
+from tensorflow.keras.metrics import CategoricalAccuracy, Mean, Precision, Recall, AUC
 from tensorflow.keras.optimizers import Adam, RMSprop, Adagrad, SGD, Adadelta, Nadam
+
 
 def get_optimizer(learning_rate, opt_name):
     '''Select optimizer method
@@ -24,3 +28,29 @@ def get_optimizer(learning_rate, opt_name):
         optimizer = Adadelta(learning_rate, rho=0.45)
 
     return optimizer
+
+
+def losses_and_metrics(num_classes):
+    """
+    Define loss and metrics
+    Loss: Categorical Crossentropy
+    Metrics: (Train, Validation) Accuracy, Precision, Recall, F1
+
+    Returns:
+        loss, train loss, train acc, valid loss, valid acc, precision, recall, auc, f1
+    """
+
+    loss_fn = CategoricalCrossentropy(from_logits=True)
+    train_loss = Mean(name='train_loss')
+    train_accuracy = CategoricalAccuracy('train_accuracy')
+
+    valid_loss = Mean(name='valid_loss')
+    valid_accuracy = CategoricalAccuracy('valid_accuracy')
+
+    precision = Precision(name='precision')
+    recall = Recall(name='recall')
+    auc = AUC(name='auc')
+    f1_train = tfa.metrics.F1Score(num_classes=num_classes, average='macro')
+    f1_loss = tfa.metrics.F1Score(num_classes=num_classes, average='macro')
+
+    return loss_fn, train_loss, train_accuracy, valid_loss, valid_accuracy, precision, recall, auc, f1_train, f1_loss
